dda0a3cb13ed3517c6d06647d466e184bccbedcd
WagnerHenrique <wagnere08@gmail.com>
2023-04-15 16:58:51 -0300

    final

--- js/scripts.js
+++ js/scripts.js
@@ -109,4 +109,37 @@ $(document).ready(function() {
         $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
         $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
     }, 250);
+
+    // Scroll para seções
+
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#carouselExampleIndicators');
+    let aboutSection = $('#about-area');
+    let serviceSection = $('#service-area');
+    let teamSection = $('#team-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+        
+        let btnId = $(this).attr('id');
+
+        if(btnId == 'about-menu'){
+        scrollTo = aboutSection;
+        } else if(btnId == 'service-menu'){
+        scrollTo = serviceSection;
+        } else if(btnId == 'team-menu'){
+        scrollTo = teamSection;
+        } else if(btnId == 'contact-menu'){
+        scrollTo = contactSection;
+        } else{
+        scrollTo = bannerSection
+        }
+
+        $([document.documentElement, document.body]).animate({scrollTop: $(scrollTo).offset().top - 70}, 150);
+
+    });
+
 });
